611da8666c254bfa7d9a0ad49eb0441f80160c7e
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-24 23:54:42 +0600

    implement smooth scroll event

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const scrollSection1 = document.getElementById("section--1");
 
 // Modal Window
 const openModal = function (e) {
@@ -21,3 +23,9 @@ const closeModal = function () {
 // show modal button
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
+
+// scroll learn more button
+btnScrollTo.addEventListener("click", function (e) {
+  const s1Scroll = scrollSection1.getBoundingClientRect();
+  scrollSection1.scrollIntoView({ behavior: "smooth" });
+});
