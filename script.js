74994354ab35af7d6090535992e2523c92813ed8
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-24 23:27:56 +0600

    close modal window by click close event

--- script.js
+++ script.js
@@ -2,12 +2,22 @@
 
 // get element by class name and Id
 const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
 
 // Modal Window
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 // show modal button
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+btnCloseModal.addEventListener("click", closeModal);
