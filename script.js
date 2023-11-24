4bddf9826b8078a7c83792170d7ebbca6ea7992a
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-24 23:09:38 +0600

    showing modal on UI by click event

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// get element by class name and Id
+const modal = document.querySelector(".modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+// Modal Window
+const openModal = function () {
+  modal.classList.remove("hidden");
+};
+
+// show modal button
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
