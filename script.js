719995ede3d6bf34ce1d359699bc7231c091b1e3
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-25 12:24:05 +0600

    add sticky class by use Intersecting Observer

--- script.js
+++ script.js
@@ -87,3 +87,23 @@ const handleHover = function (e) {
 // passing "argument" into handler
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky navigation: Intersection Observer API
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+// console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const handleObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+handleObserver.observe(header);
