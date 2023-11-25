d9bf392ac07ec89cdb55460505c8cf460506f039
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-25 12:44:12 +0600

    remove hidden section class by using Intersection Observer

--- script.js
+++ script.js
@@ -107,3 +107,26 @@ const handleObserver = new IntersectionObserver(stickyNav, {
 });
 
 handleObserver.observe(header);
+
+// Revel sections
+const allSections = document.querySelectorAll(".section");
+
+const revelSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revelSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
